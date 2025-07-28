b846e8579e0ad8acfbe92f90bc9d8a0a0acdcab8
Abhipriyo Kabi <officialkabi03@gmail.com>
2025-07-28 15:32:06 +0530

    Loggigng and Exception Handling

---
+++ app.py
@@ -0,0 +1,15 @@
+from src.MLProject.logger import logging
+from src.MLProject.exception import CustomException
+import sys
+
+
+
+if __name__== "__main__":
+    logging.info("Starting the application")
+
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Custom Exception")
+        raise CustomException(e, sys)    
+        
